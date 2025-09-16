d7640831b60abcf3f1fbd98a15c24308428a4976
Jesua <180544338+jesua-cell@users.noreply.github.com>
2025-09-16 18:20:22 -0400

    Levantamiento del Servidor y Configuracion y Conexion a la BD

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+// Dedfinir la app
+import express, { Request, Response } from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Middelware
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Mondongo");
+});
+
+app.listen(PORT, () => {
+    console.log(`Servivor corriendo en el puerto: ${PORT}`);
+});
+
+// instalas ts-node: herramienta para ejecutar el codigo ty sin compilar en js
+// npm i -g nodemon: Reinicia el servidor de forma global
+// ejecutar nodemon en la consola: nodemon .\src\app.ts
+// Configurar el package.json: agregarle esta linea => "start": "nodemon ./src/app.ts"
---
+++ src/database/database.ts
@@ -0,0 +1,41 @@
+// Crear la conexion:
+// Crear funciones para la consulta:
+import mysql2, { ConnectionOptions, ResultSetHeader, RowDataPacket } from "mysql2/promise";
+
+// Configuracion:
+
+const config: ConnectionOptions = {
+    host: '',
+    user: 'root',
+    password: '',
+    database: '',
+    connectionLimit: 10,
+    waitForConnections: true,
+    queueLimit: 0
+}
+
+// Pool de conexiones: Gestionara las conexiones a la BD
+
+const pool = mysql2.createPool(config);
+
+// Ejecutas la consultas de la BD:
+
+class DataBase {
+    async query<T extends RowDataPacket[] | ResultSetHeader>(sql: string, values: any = null) {
+
+        const cn = await pool.getConnection(); // conexion
+        try {
+            const [results] = await cn.query(sql, values); // resultados
+            return results as T;
+        } finally {
+            cn.release(); // Liberar la conexion
+        };
+    };
+};
+
+export default new DataBase(); // Exportar una instancia
+
+
+// Descargar mysql2
+
+//https://youtu.be/7ExWCTikwOg?t=1096
